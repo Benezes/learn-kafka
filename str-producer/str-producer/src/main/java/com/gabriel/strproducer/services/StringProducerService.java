73d46973f6b73ea4f0b32405bacfdd8e4fd02e42
Benezes <gabriel.menezes3@hotmail.com>
2022-03-13 14:22:19 -0300

    add a call back in send message method

--- str-producer/str-producer/src/main/java/com/gabriel/strproducer/services/StringProducerService.java
+++ str-producer/str-producer/src/main/java/com/gabriel/strproducer/services/StringProducerService.java
@@ -1,16 +1,27 @@
 package com.gabriel.strproducer.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@Log4j2
 @RequiredArgsConstructor
 public class StringProducerService {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message){
-        kafkaTemplate.send("str-topic", message);
+        kafkaTemplate.send("str-topic", message).addCallback(
+                success -> {
+                    if(success != null) {
+                        log.info("Send message with success {} ", message);
+                        log.info("Partition {} ", success.getRecordMetadata().partition());
+                        log.info("Offset {} ", success.getRecordMetadata().offset());
+                    }
+                },
+                error -> log.error("Error sending message")
+        );
     }
 }
